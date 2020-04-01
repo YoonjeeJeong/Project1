fea216a04f209a14728a5f3bcd0f6bfd1c5f72f8
Kosmo_27 <Kosmo_27@DESKTOP-28UC6QS>
2020-04-01 11:09:44 +0900

    4/1만우절 커밋!

--- src/Project1/PhoneBookVer02.java
+++ src/Project1/PhoneBookVer02.java
@@ -3,32 +3,6 @@ import java.util.Scanner;
 import ver02.PhoneInfo;
 
 public class PhoneBookVer02 {
-/*
- 프로그램 사용자로부터 데이터를 입력받는 형태로 변경하시오.
-단 반복문을 이용해서 프로그램의 흐름이 계속 유지되도록 해야한다. 
-사용자가 종료를 명령하지 않으면 다음과 같은 과정이 반복적으로 이루어진다.
-키보드를 통한 데이터입력 -> 생성자를 통한 인스턴스생성 -> 
-showPhoneInfo() 메소드호출
-매 실행시 마다 기존 생성된 인스턴스는 유지되지 않아도 된다.(다음단계에서 진행)
-실행결과>
-선택하세요...
-1. 데이터 입력
-2. 프로그램 종료
-선택: 1
-이름: 성낙현
-전화번호: 010-7906-3600
-생년월일: 75.11.30
- 
-입력된 정보 출력...
-name: 성낙현
-phone: 010-7906-3600
-birth: 75.11.30
- 
-선택하세요...
-1. 데이터 입력
-2. 프로그램 종료
-선택: 2
-프로그램을 종료합니다.*/
 
 	public static void main(String[] args) {
 		
@@ -43,6 +17,7 @@ birth: 75.11.30
 					int choice = scan.nextInt();
 					scan.nextLine();
 					
+					//생일을 입력하지 않아도 에러가 나지 않도록 if절로 옵션을 준다.
 					if(choice==1) {
 						System.out.println("선택 1");
 						System.out.print("이름을 입력하세요: ");
@@ -57,16 +32,19 @@ birth: 75.11.30
 							PhoneInfo person = 
 									new PhoneInfo(name, phoneNumber, birthday);
 							person.showPhoneInfo();
-						}
+					}
+					//프로그램 종료
 					else if(choice==2) {
 					System.out.println("선택 2");
 					System.out.println("프로그램이 종료되었습니다");
 					System.out.println("==================");
+					break;
 					}
+					//문자를 입력하거나 숫자를 잘못 입력했을 경우
 					else {
 						System.out.println("잘못 선택하셨어요");
 					}
-				}
+				}//숫자를 입력하라고 말함
 				catch (NumberFormatException e) {
 					System.out.println("숫자를 입력하세요");
 					return;
